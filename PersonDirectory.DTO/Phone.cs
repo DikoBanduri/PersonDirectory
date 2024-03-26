76433cbc04f9d64dd5f7504a76b4655b85f0c3da
DikoBanduri <Diko.banduri@gmail.com>
2024-03-27 00:44:18 +0400

    add type in phone DTO

--- PersonDirectory.DTO/Phone.cs
+++ PersonDirectory.DTO/Phone.cs
@@ -3,11 +3,14 @@
 public class Phone
 {
     public int Id { get; set; }
-    public string Personal { get; set; } = null!;
-    public string? Home { get; set; }
-    public string? Office { get; set; }
-    public string? Additional { get; set; }
+    public PhoneType PhoneType { get; set; }
     public DateTime CreateDate { get; set; }
     public bool IsDelete { get; set; }
     public Person? People { get; set; }
+}
+public enum PhoneType : byte
+{
+    Personal = 0,
+    Office = 1,
+    Home = 2
 }
--- PersonDirectory.Repository/DirectoryDbContext.cs
+++ PersonDirectory.Repository/DirectoryDbContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Query.Internal;
 using PersonDirectory.DTO;
 
 namespace PersonDirectory.Repository;
@@ -26,28 +25,26 @@ public class DirectoryDbContext : DbContext
         modelBuilder.Entity<City>().HasIndex(c => c.Name).IsUnique(true);
         modelBuilder.Entity<City>().Property(c => c.CreateDate).HasColumnType("datetime");
         modelBuilder.Entity<City>().Property(c => c.IsDelete).HasColumnType("bit");
-        modelBuilder.Entity<City>().HasMany(c => c.People).WithOne(c => c.City);
+        modelBuilder.Entity<City>().HasMany(c => c.People).WithOne(c => c.City).OnDelete(DeleteBehavior.NoAction);
 
         modelBuilder.Entity<Person>().Property(p => p.FirstName).HasColumnType("nvarchar").HasMaxLength(50).IsRequired(true).HasAnnotation("MinLength", 2);
         modelBuilder.Entity<Person>().Property(p => p.LastName).HasColumnType("nvarchar").IsRequired(true).HasMaxLength(50).HasAnnotation("MinLength", 2);
         modelBuilder.Entity<Person>().Property(p => p.PIN).HasColumnType("nvarchar(11)").IsRequired(true);
         modelBuilder.Entity<Person>().Property(p => p.BirthDate).IsRequired(true).HasColumnType("date").HasAnnotation("CheckConstraint", "DateOfBirth <= DateAdd(Year, -18, GetDate())");
-        modelBuilder.Entity<Person>().HasMany(c => c.PhoneNumbers).WithOne(c => c.People);
         modelBuilder.Entity<Person>().Property(p => p.CreateDate).IsRequired(true).HasColumnType("datetime");
         modelBuilder.Entity<Person>().Property(p => p.IsDelete).IsRequired(true).HasColumnType("Bit");
+        modelBuilder.Entity<Person>().HasMany(c => c.PhoneNumbers).WithOne(c => c.People).OnDelete(DeleteBehavior.NoAction);
 
-        modelBuilder.Entity<Relation>().Property(r => r.Type).IsRequired(true);
+
+        modelBuilder.Entity<Relation>().Property(r => r.Type).HasColumnType("nvarchar").IsRequired(true);
         modelBuilder.Entity<Relation>().Property(p => p.CreateDate).IsRequired(true).HasColumnType("datetime");
         modelBuilder.Entity<Relation>().Property(p => p.IsDelete).IsRequired(true).HasColumnType("Bit");
         modelBuilder.Entity<Relation>().HasOne(r => r.FromPerson).WithMany(p => p.FromRelations).HasForeignKey(r => r.FromId).OnDelete(DeleteBehavior.NoAction);
         modelBuilder.Entity<Relation>().HasOne(r => r.ToPerson).WithMany(p => p.ToRelations).HasForeignKey(r => r.ToId).OnDelete(DeleteBehavior.NoAction);
 
-        modelBuilder.Entity<Phone>().Property(p => p.Personal).HasColumnType("nvarchar").IsRequired(true).HasMaxLength(50).HasAnnotation("MinLength", 4);
-        modelBuilder.Entity<Phone>().Property(p => p.Home).HasColumnType("nvarchar").IsRequired(false).HasMaxLength(50).HasAnnotation("MinLength", 4);
-        modelBuilder.Entity<Phone>().Property(p => p.Office).HasColumnType("nvarchar").IsRequired(false).HasMaxLength(50).HasAnnotation("MinLength", 4);
-        modelBuilder.Entity<Phone>().Property(p => p.Additional).HasColumnType("nvarchar").IsRequired(false).HasMaxLength(50).HasAnnotation("MinLength", 4);
+        modelBuilder.Entity<Phone>().Property(p => p.PhoneType).HasColumnType("nvarchar").IsRequired().HasMaxLength(50).HasAnnotation("MinLength", 4);
         modelBuilder.Entity<Phone>().Property(p => p.IsDelete).HasColumnType("bit");
         modelBuilder.Entity<Phone>().Property(p => p.CreateDate).HasColumnType("datetime");
-        modelBuilder.Entity<Phone>().HasOne(p => p.People).WithMany(p => p.PhoneNumbers);
+        modelBuilder.Entity<Phone>().HasOne(p => p.People).WithMany(p => p.PhoneNumbers).OnDelete(DeleteBehavior.NoAction);
     }
 }
